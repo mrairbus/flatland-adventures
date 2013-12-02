64a3c41152ee193b7882391cf260d0e0d7fe51e0
Andreayquemas <andreacaritasonriente@gmail.com>
2013-12-02 17:16:52 +0100

    Función pausa
    Pausa el juego cuando se presiona la tecla P. Cuando se vuelve a presionar, se vuelve al juego. Hemos añadido como funciones: "jugar"

--- game.js
+++ game.js
@@ -11,6 +11,20 @@ var ovejas = [
 	233, 245
 ];
 
+var pausado = false;
+
+function jugar() {
+	if(pausado) {
+		pintor.fillStyle = "#fff";
+		pintor.fillRect(0, 0, 100, 30);
+		pintor.font = "20px Georgia";
+		pintor.fillStyle = "pink";
+		pintor.fillText("Pausado", 20, 20);
+	} else {
+		pintar();
+	}
+}
+
 function pintar() {
 pintor.fillStyle = "pink";
 pintor.fillRect(0, 0, 800, 600);
@@ -32,6 +46,15 @@ pintor.fillRect(x, y, 25, 25);
 }
 
 window.addEventListener("keydown", function(event) {
+											
+if (pausado) {
+	
+	if (event.keyCode == 80) {
+		pausado = false;
+	}
+	
+}
+else {
 	if(event.keyCode == 87) {
 		y = y - 25;
 	}
@@ -44,6 +67,17 @@ window.addEventListener("keydown", function(event) {
 	if(event.keyCode == 83) {
 		y = y + 25;
 	}
+	if(event.keyCode == 80) {
+		if (pausado) {
+			pausado = false;
+		}
+		else
+		{
+			pausado = true;
+		}
+	}
+}
 });
 
-window.setInterval(pintar, 25);
+
+window.setInterval(jugar, 25);
