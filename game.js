b67a7112e89cc8815ab4eb0ef48230d2507cce1e
Antoniohrm <h.rodicio@hotmail.com>
2013-12-02 17:32:53 +0100

    Añadiendo coyotes

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ var pintor = canvas.getContext("2d");
 var x = 200;
 var y = 240;
 
+var lobos = [
+	70, 70,
+	100, 127
+];
+
 var ovejas = [
 	25, 30,
 	50, 50,
@@ -12,7 +17,7 @@ var ovejas = [
 ];
 
 function pintar() {
-pintor.fillStyle = "pink";
+pintor.fillStyle = "magenta";
 pintor.fillRect(0, 0, 800, 600);
 
 pintor.fillStyle = "#fff";
@@ -27,9 +32,17 @@ ovejas[(2*oveja)+1],
 
 }
 
-pintor.fillStyle = "#f50";
-pintor.fillRect(x, y, 25, 25);
+pintor.fillStyle = "green";
+pintor.fillRect(x, y, 19, 20);
+
+pintor.fillStyle = "green";
+for(lobo=0; lobo<(lobos.length/2); lobo++){
+pintor.fillRect(
+lobos[2*lobo],
+lobos[(2*lobo)+1],13,13);
 }
+}
+
 
 window.addEventListener("keydown", function(event) {
 	if(event.keyCode == 87) {
@@ -44,6 +57,19 @@ window.addEventListener("keydown", function(event) {
 	if(event.keyCode == 83) {
 		y = y + 25;
 	}
+	for(lobo=0; lobo<(lobos.length/2); lobo++){
+	lobos[2*lobo] = lobos[2*lobo] - 15+Math.floor(Math.random()*30);
+	lobos[2*lobo+1] = lobos[2*lobo+1] - 15+Math.floor(Math.random()*30);
+	if(lobos[2*lobo]<0){
+		lobos[2*lobo]=lobos[2*lobo]+50
+	}
+	if(lobos[2*lobo+1]<0){
+	lobos[2*lobo+1]=lobos[2*lobo+1]+50
+	}
+	}
 });
 
-window.setInterval(pintar, 25);
+
+
+
+window.setInterval(pintar, 25); 
